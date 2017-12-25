262a6b9f161a0bd954ff3ec851636cd9f748c4f9
jraju9 <jraju9@gmail.com>
2017-12-25 15:13:11 -0500

    add existign file

---
+++ install_3pd.sh
@@ -0,0 +1,18 @@
+# install google chrome
+
+get -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+sudo apt-get update
+sudo apt-get install google-chrome-stable
+
+
+
+# install sublime3 for current user
+
+
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-text
+
+
