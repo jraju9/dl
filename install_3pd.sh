dfaf8aac5b7cdb56d55a44e01cf7a80469f13efd
jraju9 <jraju9@gmail.com>
2020-03-12 12:38:15 -0400

    Added ffmpeg

--- install_3pd.sh
+++ install_3pd.sh
@@ -37,3 +37,20 @@ sudo apt-get install pavucontrol
 
 #To open pavucontrol from the Terminal:
 pavucontrol
+
+
+#To install ffmpeg - required for animation or video conversion
+sudo apt update
+sudo apt install ffmpeg
+
+#To validate that the package is installed properly use the ffmpeg -version command which prints the FFmpeg version:
+fmpeg -version
+
+#jai@ai-hub:~$ ffmpeg -version
+#ffmpeg version 3.4.6-0ubuntu0.18.04.1 Copyright (c) 2000-2019 the FFmpeg developers
+#built with gcc 7 (Ubuntu 7.3.0-16ubuntu3) configuration: --prefix=/usr --extra-version=0ubuntu0.18.04.1 --toolchain=hardened --libdir=/usr/lib/x86_64-linux-gnu --incdir=/usr/include/x86_64-linux-gnu
+
+#To print all available FFmpeg’s encoders and decoders type:
+
+ffmpeg -encoders
+ffmpeg -decoders
